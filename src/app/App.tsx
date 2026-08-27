cf792ce0306e68c10e91f108473ff6686d633630
Max <hamidzonovmuhriddin@gmail.com>
2026-08-27 17:21:43 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import "../css/app.css";
 import { Container } from "@mui/material";
 import { Link, Route, Switch } from "react-router-dom";
-import { Users } from "./screens/Users";
-import { About } from "./screens/About";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/usersPage/Index";
+import { HomePage } from "./screens/homePage";
 
 function App() {
   return (
@@ -10,26 +12,32 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HomePage</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/products">ProductsPage</Link>{" "}
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
           </li>
         </ul>
       </nav>
 
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/products">
+          <ProductsPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
         </Route>
         <Route path="/">
-          <Home />
+          <HomePage />
         </Route>
       </Switch>
     </div>
