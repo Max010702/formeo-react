9dd29dd7c8faf17ebac146ec85cb89141d7c1378
Max <hamidzonovmuhriddin@gmail.com>
2026-08-29 21:22:26 +0900

    fix: organize homepage related sectional components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,12 @@ import ProductsPage from "./screens/productsPage";
 import OrdersPage from "./screens/ordersPage";
 import UserPage from "./screens/usersPage/index";
 import HomePage from "./screens/homePage";
+import HelpPage from "./screens/helpPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
-import HelpPage from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,19 @@
-import { Container } from "@mui/material";
+import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
+import Events from "./Events";
+import NewDishes from "./NewProducts";
+import PopularDishes from "./PopularProducts";
+import Statistics from "./Statistic";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
