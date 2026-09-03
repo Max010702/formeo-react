90192b7fa83befc8a2dde1a38b486ba56a0e65d9
Max <hamidzonovmuhriddin@gmail.com>
2026-09-03 20:05:56 +0900

    fix: modify ProcessOrders and FinishedOrders components

--- src/app/screens/ordersPage/FinishedOrders.tsx
+++ src/app/screens/ordersPage/FinishedOrders.tsx
@@ -1,211 +1,118 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import TabPanel from "@mui/lab/TabPanel";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
-import ReplayOutlinedIcon from "@mui/icons-material/ReplayOutlined";
-import "../../../css/order.css";
+import { useSelector } from "react-redux";
 
-interface OrderProduct {
-  id: number;
-  name: string;
-  image: string;
-  material: string;
-  price: number;
-  quantity: number;
-}
-
-interface FinishedOrder {
-  id: string;
-  completedAt: string;
-  products: OrderProduct[];
-  delivery: number;
-}
-
-const finishedOrders: FinishedOrder[] = [
-  {
-    id: "FRM-2026-0418",
-    completedAt: "August 22, 2026",
-    delivery: 25,
-    products: [
-      {
-        id: 1,
-        name: "Cloud Modular Sofa",
-        image: "/img/sofa.webp",
-        material: "Natural ivory bouclé",
-        price: 1890,
-        quantity: 1,
-      },
-      {
-        id: 2,
-        name: "Nova Coffee Table",
-        image: "/img/coffee-table.webp",
-        material: "Natural travertine",
-        price: 760,
-        quantity: 1,
-      },
-    ],
-  },
-  {
-    id: "FRM-2026-0327",
-    completedAt: "August 5, 2026",
-    delivery: 25,
-    products: [
-      {
-        id: 3,
-        name: "Cane Lounge Chair",
-        image: "/img/lounge-chair.webp",
-        material: "Oak and natural cane",
-        price: 680,
-        quantity: 2,
-      },
-    ],
-  },
-];
+import { retrieveFinishedOrders } from "./selector";
+import { serverApi } from "../../lib/config";
 
 export default function FinishedOrders() {
+  const finishedOrders = useSelector(retrieveFinishedOrders);
+
   return (
     <TabPanel value="3">
-      <Box className="finished-orders">
-        {finishedOrders.length > 0 ? (
-          <Stack className="finished-orders__list">
-            {finishedOrders.map((order) => {
-              const subtotal = order.products.reduce(
-                (total, product) => total + product.price * product.quantity,
-                0,
-              );
+      <Stack>
+        {finishedOrders.map((order) => (
+          <Box key={order._id} className="order-main-box">
+            <Box className="order-box-scroll">
+              {order.orderItems.map((item) => {
+                const product = order.productData.find(
+                  (productItem) => productItem._id === item.productId,
+                );
 
-              const total = subtotal + order.delivery;
+                if (!product) {
+                  return null;
+                }
 
-              return (
-                <Box className="finished-order" key={order.id}>
-                  <Stack className="finished-order__header">
-                    <Stack className="finished-order__identity">
-                      <Box className="finished-order__status-icon">
-                        <CheckCircleOutlineIcon />
-                      </Box>
+                const productImage = product.productImages[0];
 
-                      <Box>
-                        <Box className="finished-order__eyebrow">
-                          Completed order
-                        </Box>
+                const imagePath = productImage
+                  ? productImage.startsWith("http")
+                    ? productImage
+                    : `${serverApi}/${productImage}`
+                  : "/icons/noimage-list.svg";
 
-                        <Box className="finished-order__number">
-                          Order #{order.id}
-                        </Box>
-                      </Box>
-                    </Stack>
+                const itemTotal = item.itemQuantity * item.itemPrice;
 
-                    <Box className="finished-order__date">
-                      Delivered on {order.completedAt}
+                return (
+                  <Box key={item._id} className="orders-name-price">
+                    <img
+                      src={imagePath}
+                      className="order-dish-img"
+                      alt={product.productName}
+                    />
+
+                    <p className="title-dish">{product.productName}</p>
+
+                    <Box className="price-box">
+                      <p>${item.itemPrice.toLocaleString()}</p>
+
+                      <img src="/icons/close.svg" alt="Times" />
+
+                      <p>{item.itemQuantity}</p>
+
+                      <img src="/icons/pause.svg" alt="Equals" />
+
+                      <p style={{ marginLeft: "15px" }}>
+                        ${itemTotal.toLocaleString()}
+                      </p>
                     </Box>
-                  </Stack>
-
-                  <Stack className="finished-order__products">
-                    {order.products.map((product) => (
-                      <Box className="finished-order__product" key={product.id}>
-                        <Box className="finished-order__image-wrapper">
-                          <img
-                            src={product.image}
-                            alt={product.name}
-                            className="finished-order__image"
-                          />
-                        </Box>
-
-                        <Stack className="finished-order__product-info">
-                          <Box>
-                            <Box className="finished-order__product-name">
-                              {product.name}
-                            </Box>
-
-                            <Box className="finished-order__material">
-                              {product.material}
-                            </Box>
-                          </Box>
-
-                          <Box className="finished-order__quantity">
-                            Quantity: {product.quantity}
-                          </Box>
-                        </Stack>
-
-                        <Stack className="finished-order__price-info">
-                          <Box className="finished-order__unit-price">
-                            ${product.price.toLocaleString()} each
-                          </Box>
-
-                          <Box className="finished-order__product-total">
-                            $
-                            {(
-                              product.price * product.quantity
-                            ).toLocaleString()}
-                          </Box>
-                        </Stack>
-                      </Box>
-                    ))}
-                  </Stack>
-
-                  <Stack className="finished-order__summary">
-                    <Stack className="finished-order__actions">
-                      <Button
-                        variant="outlined"
-                        startIcon={<ReplayOutlinedIcon />}
-                        className="finished-order__secondary-button"
-                      >
-                        Order again
-                      </Button>
-
-                      <Button
-                        variant="text"
-                        startIcon={<LocalShippingOutlinedIcon />}
-                        className="finished-order__track-button"
-                      >
-                        Delivery details
-                      </Button>
-                    </Stack>
-
-                    <Stack className="finished-order__totals">
-                      <Stack className="finished-order__total-row">
-                        <span>Products</span>
-                        <strong>${subtotal.toLocaleString()}</strong>
-                      </Stack>
-
-                      <Stack className="finished-order__total-row">
-                        <span>Delivery</span>
-                        <strong>${order.delivery.toLocaleString()}</strong>
-                      </Stack>
-
-                      <Stack className="finished-order__grand-total">
-                        <span>Total</span>
-                        <strong>${total.toLocaleString()}</strong>
-                      </Stack>
-                    </Stack>
-                  </Stack>
-                </Box>
-              );
-            })}
-          </Stack>
-        ) : (
-          <Stack className="finished-orders__empty">
-            <Box className="finished-orders__empty-icon">
-              <CheckCircleOutlineIcon />
+                  </Box>
+                );
+              })}
             </Box>
 
-            <Box className="finished-orders__empty-title">
-              No completed orders
-            </Box>
+            <Box className="total-price-box">
+              <Box className="box-total">
+                <p>Product price</p>
 
-            <Box className="finished-orders__empty-text">
-              Furniture orders you have received will appear here.
-            </Box>
+                <p>
+                  ${(order.orderTotal - order.orderDelivery).toLocaleString()}
+                </p>
 
-            <Button
-              variant="contained"
-              className="finished-orders__shop-button"
-            >
-              Explore collection
-            </Button>
-          </Stack>
+                <img
+                  src="/icons/plus.svg"
+                  alt="Plus"
+                  style={{ marginLeft: "20px" }}
+                />
+
+                <p>Delivery cost</p>
+
+                <p>${order.orderDelivery.toLocaleString()}</p>
+
+                <img
+                  src="/icons/pause.svg"
+                  alt="Equals"
+                  style={{ marginLeft: "20px" }}
+                />
+
+                <p>Total</p>
+
+                <p>${order.orderTotal.toLocaleString()}</p>
+              </Box>
+            </Box>
+          </Box>
+        ))}
+
+        {finishedOrders.length === 0 && (
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <img
+              src="/icons/noimage-list.svg"
+              style={{
+                width: 300,
+                height: 300,
+              }}
+              alt="No finished orders"
+            />
+
+            <Box className="no-data">No finished orders</Box>
+          </Box>
         )}
-      </Box>
+      </Stack>
     </TabPanel>
   );
 }
--- src/app/screens/ordersPage/ProcessOrders.tsx
+++ src/app/screens/ordersPage/ProcessOrders.tsx
@@ -1,195 +1,127 @@
-import { Box, Button, LinearProgress, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import TabPanel from "@mui/lab/TabPanel";
-import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
-import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-import SupportAgentOutlinedIcon from "@mui/icons-material/SupportAgentOutlined";
-import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
 import moment from "moment";
-import "../../../css/order.css";
+import { useSelector } from "react-redux";
+
+import { retrieveProcessOrders } from "./selector";
+import { serverApi } from "../../lib/config";
 
 export default function ProcessOrders() {
+  const processOrders = useSelector(retrieveProcessOrders);
+
   return (
     <TabPanel value="2">
-      <Box className="process-orders">
-        {[1, 2].length > 0 ? (
-          <Stack className="process-orders__list">
-            {[1, 2].map((_order, index) => (
-              <Box className="process-order" key={index}>
-                <Stack className="process-order__header">
-                  <Stack className="process-order__identity">
-                    <Box className="process-order__status-icon">
-                      <LocalShippingOutlinedIcon />
+      <Stack>
+        {processOrders.map((order) => (
+          <Box key={order._id} className="order-main-box">
+            <Box className="order-box-scroll">
+              {order.orderItems.map((item) => {
+                const product = order.productData.find(
+                  (productItem) => productItem._id === item.productId,
+                );
+
+                if (!product) {
+                  return null;
+                }
+
+                const productImage = product.productImages[0];
+
+                const imagePath = productImage
+                  ? productImage.startsWith("http")
+                    ? productImage
+                    : `${serverApi}/${productImage}`
+                  : "/icons/noimage-list.svg";
+
+                const itemTotal = item.itemQuantity * item.itemPrice;
+
+                return (
+                  <Box key={item._id} className="orders-name-price">
+                    <img
+                      src={imagePath}
+                      className="order-dish-img"
+                      alt={product.productName}
+                    />
+
+                    <p className="title-dish">{product.productName}</p>
+
+                    <Box className="price-box">
+                      <p>${item.itemPrice.toLocaleString()}</p>
+
+                      <img src="/icons/close.svg" alt="Times" />
+
+                      <p>{item.itemQuantity}</p>
+
+                      <img src="/icons/pause.svg" alt="Equals" />
+
+                      <p style={{ marginLeft: "15px" }}>
+                        ${itemTotal.toLocaleString()}
+                      </p>
                     </Box>
-
-                    <Box>
-                      <Box className="process-order__eyebrow">
-                        Order in progress
-                      </Box>
-
-                      <Box className="process-order__number">
-                        Order #FRM-2026-0{498 - index}
-                      </Box>
-                    </Box>
-                  </Stack>
-
-                  <Box className="process-order__delivery-date">
-                    <span>Estimated delivery</span>
-
-                    <strong>
-                      {moment()
-                        .add(index + 3, "days")
-                        .format("MMMM D, YYYY")}
-                    </strong>
                   </Box>
-                </Stack>
+                );
+              })}
+            </Box>
 
-                <Box className="process-order__progress">
-                  <Stack className="process-order__progress-heading">
-                    <Box>
-                      {index === 0
-                        ? "Preparing your furniture"
-                        : "Out for delivery"}
-                    </Box>
+            <Box className="total-price-box">
+              <Box className="box-total">
+                <p>Product price</p>
 
-                    <span>{index === 0 ? 45 : 82}%</span>
-                  </Stack>
+                <p>
+                  ${(order.orderTotal - order.orderDelivery).toLocaleString()}
+                </p>
 
-                  <LinearProgress
-                    variant="determinate"
-                    value={index === 0 ? 45 : 82}
-                    className="process-order__progress-bar"
-                  />
+                <img
+                  src="/icons/plus.svg"
+                  alt="Plus"
+                  style={{ marginLeft: "20px" }}
+                />
 
-                  <Stack className="process-order__progress-labels">
-                    <span>Confirmed</span>
-                    <span>Preparing</span>
-                    <span>Delivery</span>
-                    <span>Completed</span>
-                  </Stack>
-                </Box>
+                <p>Delivery cost</p>
 
-                <Stack className="process-order__products">
-                  {[1, 2].map((_product, index2) => (
-                    <Box className="process-order__product" key={index2}>
-                      <Box className="process-order__image-wrapper">
-                        <img
-                          src={
-                            index2 === 0
-                              ? "/img/sofa.webp"
-                              : "/img/coffee-table.webp"
-                          }
-                          className="process-order__image"
-                          alt={
-                            index2 === 0
-                              ? "Cloud Modular Sofa"
-                              : "Nova Coffee Table"
-                          }
-                        />
-                      </Box>
+                <p>${order.orderDelivery.toLocaleString()}</p>
 
-                      <Stack className="process-order__product-info">
-                        <Box>
-                          <Box className="process-order__product-name">
-                            {index2 === 0
-                              ? "Cloud Modular Sofa"
-                              : "Nova Coffee Table"}
-                          </Box>
+                <img
+                  src="/icons/pause.svg"
+                  alt="Equals"
+                  style={{ marginLeft: "20px" }}
+                />
 
-                          <Box className="process-order__material">
-                            {index2 === 0
-                              ? "Natural ivory bouclé"
-                              : "Natural travertine"}
-                          </Box>
-                        </Box>
+                <p>Total</p>
 
-                        <Box className="process-order__quantity">
-                          Quantity: 1
-                        </Box>
-                      </Stack>
-
-                      <Stack className="process-order__price-info">
-                        <Box className="process-order__unit-price">
-                          {index2 === 0 ? "$1,890 each" : "$760 each"}
-                        </Box>
-
-                        <Box className="process-order__product-total">
-                          {index2 === 0 ? "$1,890" : "$760"}
-                        </Box>
-                      </Stack>
-                    </Box>
-                  ))}
-                </Stack>
-
-                <Stack className="process-order__summary">
-                  <Stack className="process-order__delivery-info">
-                    <Stack className="process-order__address">
-                      <LocationOnOutlinedIcon />
-
-                      <Box>
-                        <span>Delivery address</span>
-                        <strong>Seoul, South Korea</strong>
-                      </Box>
-                    </Stack>
-
-                    <Stack className="process-order__actions">
-                      <Button
-                        variant="contained"
-                        startIcon={<LocalShippingOutlinedIcon />}
-                        className="process-order__track-button"
-                      >
-                        Track delivery
-                      </Button>
-
-                      <Button
-                        variant="text"
-                        startIcon={<SupportAgentOutlinedIcon />}
-                        className="process-order__support-button"
-                      >
-                        Get help
-                      </Button>
-                    </Stack>
-                  </Stack>
-
-                  <Stack className="process-order__totals">
-                    <Stack className="process-order__total-row">
-                      <span>Products</span>
-                      <strong>$2,650</strong>
-                    </Stack>
-
-                    <Stack className="process-order__total-row">
-                      <span>Delivery</span>
-                      <strong>$25</strong>
-                    </Stack>
-
-                    <Stack className="process-order__grand-total">
-                      <span>Total</span>
-                      <strong>$2,675</strong>
-                    </Stack>
-                  </Stack>
-                </Stack>
+                <p>${order.orderTotal.toLocaleString()}</p>
               </Box>
-            ))}
-          </Stack>
-        ) : (
-          <Stack className="process-orders__empty">
-            <Box className="process-orders__empty-icon">
-              <Inventory2OutlinedIcon />
-            </Box>
 
-            <Box className="process-orders__empty-title">
-              No orders in progress
-            </Box>
+              <p className="data-compl">
+                {moment(order.updatedAt).format("YY-MM-DD HH:mm")}
+              </p>
 
-            <Box className="process-orders__empty-text">
-              Furniture being prepared or delivered will appear here.
+              <Button variant="contained" className="verify-button">
+                Verify to Fulfill
+              </Button>
             </Box>
+          </Box>
+        ))}
 
-            <Button variant="contained" className="process-orders__shop-button">
-              Explore collection
-            </Button>
-          </Stack>
+        {processOrders.length === 0 && (
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <img
+              src="/icons/noimage-list.svg"
+              style={{
+                width: 300,
+                height: 300,
+              }}
+              alt="No processing orders"
+            />
+
+            <Box className="no-data">No processing orders</Box>
+          </Box>
         )}
-      </Box>
+      </Stack>
     </TabPanel>
   );
 }
